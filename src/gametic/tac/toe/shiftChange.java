1bd3e0eca7b63f3d009bca81fef9aebf57c332b6
Lydan186 <dilanchava48gmail.com>
2022-10-02 17:58:49 -0600

    se agrega el juego del gato

---
+++ src/gametic/tac/toe/shiftChange.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gametic.tac.toe;
+
+/**
+ *
+ * @author dilan
+ */
+public class shiftChange {
+
+    public static void shift() {
+        String turn = "X";
+        if (turn.equals("X")) {
+            turn = "O";
+
+        } else {
+            turn = "X";
+            
+        }
+
+    }
+}
